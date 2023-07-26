47890221086588f1b808d5834ad48e4fd0e704b5
Anatoly Karpov <karpik-ne@yandex.ru>
2023-07-26 15:36:13 +0700

    MoviesCard

---
+++ src/components/Movies/MoviesCard/MoviesCard.js
@@ -0,0 +1,27 @@
+import movie from '../../../images/movie.png';
+import saved from '../../../images/saved.png';
+import './MoviesCard.css'
+
+const MoviesCard = () => {
+
+    return (
+        <div className="moviescard">
+            <h2 className="moviescard__name">33 слова о дизайне</h2>
+            <img className="moviescard__saved" src={saved} alt="Сохранено" />
+            <p className="moviescard__duration">1ч 47м</p>
+            <img
+                className="moviescard__image"
+                src={movie}
+                alt="33 слова о дизайне"
+
+            />
+            
+            
+            
+
+
+        </div>
+    );
+}
+
+export default MoviesCard;
--- src/components/Movies/movies.js
+++ src/components/Movies/movies.js
@@ -2,13 +2,14 @@ import Header2 from "./Header2/Header2";
 import Footer from "../Footer/Footer";
 import Main from "../Main/Main";
 import SearchForm from "./SearchForm/SearchForm";
+import MoviesCard from "./MoviesCard/MoviesCard";
 
 const Movies = () => {
     return (
         <div>
             <Header2 />
             <SearchForm />
-           
+           <MoviesCard />
             <Footer />
         </div>
     )
