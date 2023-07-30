c0acba6862e95eaa95a9785de41cc69376ce8546
Anatoly Karpov <karpik-ne@yandex.ru>
2023-07-30 14:42:54 +0700

    320 landing

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
         <footer className="footer">
             <h3 className="footer__title">Учебный проект Яндекс.Практикум х BeatFilm.</h3>
             <div className="footer__container">
-                <p className="footer__author">&copy; 2023 Анатолий Карпов</p>
+                <p className="footer__author">&copy; 2023</p>
                 <a
                     href="https://practicum.yandex.ru"
                     className="footer__link"
