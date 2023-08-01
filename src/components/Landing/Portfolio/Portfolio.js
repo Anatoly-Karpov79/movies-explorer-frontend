ebb524db4de3184567c9cb202ed0481058b655c9
Anatoly Karpov <karpik-ne@yandex.ru>
2023-08-01 10:34:51 +0700

    fix

--- src/components/Landing/Portfolio/Portfolio.js
+++ src/components/Landing/Portfolio/Portfolio.js
@@ -7,27 +7,43 @@ const Portfolio = () => {
         <section className="portfolio" id="portfolio">
             <h2 className="portfolio__title">Портфолио</h2>
             <div className="portfolio__container">
-                <a href=" https://anatoly-karpov79.github.io/how-to-learn/" className="portfolio__link">
+                <a href=" https://anatoly-karpov79.github.io/how-to-learn/"
+                    className="portfolio__link"
+                    rel="noreferrer"
+                    target="_blank">
                     Статичный сайт
                 </a>
                 <a href="https://anatoly-karpov79.github.io/how-to-learn/"
-                    className="portfolio__arrow portfolio__link">&#129149;</a>
+                    className="portfolio__arrow portfolio__link"
+                    rel="noreferrer"
+                    target="_blank">&#129149;</a>
             </div>
             <hr className="portfolio__line"></hr>
             <div className="portfolio__container">
-                <a href="https://anatoly-karpov79.github.io/russian-travel/" className="portfolio__link">
+                <a href="https://anatoly-karpov79.github.io/russian-travel/"
+                    className="portfolio__link"
+                    rel="noreferrer"
+                    target="_blank">
                     Адаптивный сайт
                 </a>
                 <a href="https://anatoly-karpov79.github.io/russian-travel/"
-                    className="portfolio__arrow portfolio__link">&#129149;</a>
+                    className="portfolio__arrow portfolio__link"
+                    rel="noreferrer"
+                    target="_blank">&#129149;</a>
             </div>
             <hr className="portfolio__line"></hr>
             <div className="portfolio__container">
-                <a href="https://anatoly-karpov79.github.io/russian-travel/" className="portfolio__link">
+                <a href="https://anatoly-karpov79.github.io/russian-travel/"
+                    className="portfolio__link"
+                    rel="noreferrer"
+                    target="_blank">
                     Одностраничное приложение
                 </a>
                 <a href="https://anatoly-karpov79.github.io/russian-travel/"
-                    className="portfolio__arrow portfolio__link">&#129149;</a>
+                    className="portfolio__arrow portfolio__link"
+                    rel="noreferrer"
+                    target="_blank"
+                >&#129149;</a>
             </div>
         </section>
 
