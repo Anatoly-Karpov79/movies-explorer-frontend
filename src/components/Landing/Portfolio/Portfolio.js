84cec14c212c2f1514af1c6b602e9097e04ddaf7
Anatoly Karpov <karpik-ne@yandex.ru>
2023-08-03 20:39:04 +0700

    portfolio

--- src/components/Landing/Portfolio/Portfolio.js
+++ src/components/Landing/Portfolio/Portfolio.js
@@ -16,32 +16,21 @@ const Portfolio = () => {
                 <img className="portfolio__arrow" src={arrow} alt="Стрелка" />
             </a>
             <hr className="portfolio__line"></hr>
-            <div className="portfolio__container">
-                <a href="https://anatoly-karpov79.github.io/russian-travel/"
-                    className="portfolio__link"
-                    rel="noreferrer"
-                    target="_blank">
-                    Адаптивный сайт
-                </a>
-                <a href="https://anatoly-karpov79.github.io/russian-travel/"
-                    className="portfolio__arrow portfolio__link"
-                    rel="noreferrer"
-                    target="_blank">&#129149;</a>
-            </div>
+            <a href="https://anatoly-karpov79.github.io/russian-travel/"
+                className="portfolio__container"
+                rel="noreferrer"
+                target="_blank">
+                <p className="portfolio__link">Статичный сайт</p>
+                <img className="portfolio__arrow" src={arrow} alt="Стрелка" />
+            </a>
             <hr className="portfolio__line"></hr>
-            <div className="portfolio__container">
-                <a href="https://anatoly-karpov79.github.io/russian-travel/"
-                    className="portfolio__link"
-                    rel="noreferrer"
-                    target="_blank">
-                    Одностраничное приложение
-                </a>
-                <a href="https://anatoly-karpov79.github.io/russian-travel/"
-                    className="portfolio__arrow portfolio__link"
-                    rel="noreferrer"
-                    target="_blank"
-                >&#129149;</a>
-            </div>
+            <a href="https://anatoly-karpov79.github.io/russian-travel/"
+                className="portfolio__container"
+                rel="noreferrer"
+                target="_blank">
+                <p className="portfolio__link">Статичный сайт</p>
+                <img className="portfolio__arrow" src={arrow} alt="Стрелка" />
+            </a>
         </section>
 
     )
